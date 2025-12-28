6a226894853222cdcdb8411a4166ed542025a34b
dharaniruk <dharanibalamurugan2008@gmail.com>
2025-12-28 21:43:59 +0530

    Add files via upload

---
+++ lab_1/Simple_interest.java
@@ -0,0 +1,9 @@
+class Simple_interest{
+public static void main(String args[]){
+int principal=1000;
+int time=2;
+int rate=5;
+int interest=(principal*time*rate)/100;
+System.out.println("simple interest is equal to"+interest);
+}
+}
