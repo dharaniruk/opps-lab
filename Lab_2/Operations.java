cb7ac56b0fea750c561f4166ef7ed8739e9afad4
dharaniruk <dharanibalamurugan2008@gmail.com>
2025-12-29 12:36:17 +0530

    Add files via upload

---
+++ Lab_2/Operations.java
@@ -0,0 +1,21 @@
+public class Operations{
+public static void main(String args[]){
+int a=56;
+int b=64;
+int add=a+b;
+System.out.println("addition of numbers a and b is="+add);
+int sub=a-b;
+System.out.println("subtraction of numbers a and b is="+sub);
+int mul=a*b;
+System.out.println("multiplication of numbers and b is ="+mul);
+float div=a/b;
+System.out.println("division of numbers a and b is ="+div);
+double mod=a%b;
+System.out.println("modulus of number a and b is=");
+if (a>b){
+System.out.println("a is greater than b");
+}else{
+System.out.println("b is greater than a");
+}
+}
+}
