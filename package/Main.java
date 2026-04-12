0d65ef06dc79eb68167a82134f7abe54a69f7fe8
dharaniruk <dharanibalamurugan2008@gmail.com>
2026-04-12 21:05:44 +0530

    Add files via upload

---
+++ package/Main.java
@@ -0,0 +1,8 @@
+import mathoperations.Addition;
+
+public class Main {
+    public static void main(String[] args) {
+        Addition obj = new Addition();
+        System.out.println("Sum: " + obj.add(5, 3));
+    }
+}
